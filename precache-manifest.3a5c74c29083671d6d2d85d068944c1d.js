beae4bddac29bd28973086f932d4c8e33dd060a8
JGmax-zhou <66560027+JGmax-zhou@users.noreply.github.com>
2020-10-12 13:25:55 +0800

    Updates

---
+++ precache-manifest.3a5c74c29083671d6d2d85d068944c1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb9bccd265aed199e0b605aea618a5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a5b311a08d05c796ce9",
+    "url": "./static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "f325b96cf320cf03a0a8",
+    "url": "./static/css/main.4f7dc9f2.chunk.css"
+  },
+  {
+    "revision": "2a5b311a08d05c796ce9",
+    "url": "./static/js/2.c27f376d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c27f376d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f325b96cf320cf03a0a8",
+    "url": "./static/js/main.66849970.chunk.js"
+  },
+  {
+    "revision": "85402d1b17d696356fff",
+    "url": "./static/js/runtime-main.5596971f.js"
+  }
+]);
